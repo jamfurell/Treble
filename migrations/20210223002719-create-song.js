458c89e084e093df6d737d05ae3c638d788735e7
jamfurell <hong.j92@gmail.com>
2021-02-22 16:33:50 -0800

    Create models and made associations

---
+++ migrations/20210223002719-create-song.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('songs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      artist: {
+        type: Sequelize.STRING
+      },
+      album: {
+        type: Sequelize.STRING
+      },
+      lastFmId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('songs');
+  }
+};
