4d4cde2b9e4cd5d0713e1491d6334b36eacf0b5b
r-mckeith <r.mckeith@me.com>
2021-02-25 22:21:35 -0800

    update song model to account for new url column in table

--- models/song.js
+++ models/song.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       models.song.belongsToMany(models.playlist, {through: "playlistsSongs"})
+      models.song.belongsToMany(models.user, {through: "usersSongs"})
     }
   };
   song.init({
