e969d023be51a1a887aeb034f01db803d0c1cc45
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-02-26 12:33:40 -0800

    Migration files for Updated Songs Table

--- models/song.js
+++ models/song.js
@@ -11,8 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.song.belongsToMany(models.playlist, {through: "playlistsSongs"})
-      models.song.belongsToMany(models.user, {through: "usersSongs"})
     }
   };
   song.init({
