b6e6f4cbb09954417e41ff37084c239e1ff5b3e0
jamfurell <hong.j92@gmail.com>
2021-02-26 23:37:02 -0800

    accept change to song.js

--- models/song.js
+++ models/song.js
@@ -11,11 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-<<<<<<< HEAD
-=======
       models.song.belongsToMany(models.playlist, { through: "playlistsSongs" });
       models.song.belongsToMany(models.user, { through: "usersSongs" });
->>>>>>> submain
+
     }
   };
   song.init({
